7d548d435fdb496524549d32958f51b422b7e96f
mdsanaullah2030 <sanaullah1997215@gmail.com>
2024-06-24 17:06:14 +0600

    24.06.2024

--- AutoCrateTable/src/autocratetable/AutoCrateTable.java
+++ AutoCrateTable/src/autocratetable/AutoCrateTable.java
@@ -1,20 +1,99 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package autocratetable;
 
-/**
- *
- * @author Admin
- */
-public class AutoCrateTable {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import text.DbUtil;
 
-    /**
-     * @param args the command line arguments
-     */
+public class AutoCrateTable {
+    
+public static DbUtil db=new DbUtil();
+static PreparedStatement ps;
+static ResultSet rs;
+static String sql="";
     public static void main(String[] args) {
-        // TODO code application logic here
+        saveEmp("Mahabur","Mahabur@gmail.com" ,"1552478");
+        
+        showEmp();
+        deleteEmp(2);
+        System.out.println("\n aftar delete");
+        showEmp();
+        updateEmp("kutub", "kutub@gmail.com", "5214545", 1);
+        System.out.println("\n aftar update \n");
+        showEmp();
+    }
+    public  static void saveEmp(String name,String emal,String cellno ){
+    sql="insert into sanaullah(  name,emal,cellno) values(?,?,?)";
+    try {
+        ps=db.getCon().prepareStatement(sql);
+                ps.setString(1, name);
+                ps.setString(2, emal);
+                ps.setString(3, cellno);
+               ps.executeUpdate();
+               ps.close();
+               db.getCon().close();
+                } catch (SQLException ex) {
+        Logger.getLogger(AutoCrateTable.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    
+    }
+    
+    public static void showEmp(){
+    sql="select * from sanaullah";
+    try {
+        ps=db.getCon().prepareStatement(sql);
+        rs=ps.executeQuery();
+        while(rs.next()){
+        String name=rs.getString("name");
+        String emal=rs.getString("emal");
+        String cellno=rs.getString("cellno");
+        String empoyeeId=rs.getString("empoyeeId");
+        
+        
+            System.out.println("Emp ID:"+ empoyeeId+"\tName:"+name+"\tEmal"+emal+"\tcellno"+cellno);
+        
+        }
+        ps.close();
+        rs.close();
+        db.getCon().close();
+    } catch (SQLException ex) {
+        Logger.getLogger(AutoCrateTable.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    
+    
+    }
+    public static  void deleteEmp(int empoyeeId ){
+    sql="delete from sanaullah where empoyeeId=?";
+    try {
+        ps=db.getCon().prepareStatement(sql);
+        ps.setInt(1, empoyeeId);
+        ps.executeUpdate();
+        ps.close();
+        db.getCon().close();
+        
+    } catch (SQLException ex) {
+        Logger.getLogger(AutoCrateTable.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    
+    }
+    public static  void updateEmp(String name,String emal,String cellno,int empoyeeId){
+     sql="update sanaullah set name=?,emal=?,cellno=? where empoyeeId=?";
+    try {
+        ps=db.getCon().prepareStatement(sql);
+          ps.setString(1, name);
+                ps.setString(2, emal);
+                ps.setString(3, cellno);
+                ps.setInt(4, empoyeeId);
+                
+               ps.executeUpdate();
+               ps.close();
+               db.getCon().close();
+    } catch (SQLException ex) {
+        Logger.getLogger(AutoCrateTable.class.getName()).log(Level.SEVERE, null, ex);
+    }
     }
     
 }
--- AutoCrateTable/src/text/DbUtil.java
+++ AutoCrateTable/src/text/DbUtil.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package text;
 
-/**
- *
- * @author Admin
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DbUtil {
-    
+   private Connection con=null;
+   private String url="jdbc:mysql://localhost:3306/kutub";
+   private  String user="root";
+   private  String password="";
+   private String driver="com.mysql.cj.jdbc.Driver";
+   
+   public Connection getCon(){
+       try {
+           Class.forName(driver);
+           con=DriverManager.getConnection(url, user, password);
+       } catch (ClassNotFoundException ex) {
+           Logger.getLogger(DbUtil.class.getName()).log(Level.SEVERE, null, ex);
+       } catch (SQLException ex) {
+           Logger.getLogger(DbUtil.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       return con;
+   }
 }
